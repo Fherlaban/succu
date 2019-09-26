4a34e88440168790b43346621e6591a53bf8526a
Francis Miguel A. Herlaban <francismiguelherlaban@gmail.com>
2019-09-26 19:31:50 +0800

    Updated main.dart

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Succu',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+));
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
