a27f2de9a38e9b85ab48d68997581dfb72ec34e0
Francis Miguel A. Herlaban <francismiguelherlaban@gmail.com>
2019-10-04 10:51:34 +0800

    Update main.dart
    initial development for the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       title: 'Succu',
       debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.lightGreen,
+        brightness: Brightness.light,
+        primaryColor: Colors.lightGreen[800],
+        accentColor: Colors.lightGreenAccent[600],
+      ),
       home: HomeScreen(),
 ));
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      body: new Container(
+
+      ),
+    );
   }
 }
